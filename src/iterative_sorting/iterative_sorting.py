94060f5b6de0859e69fd73e915001e8c5ebece6a
Penny Tague <pmtague9@gmail.com>
2020-01-21 16:19:26 -0800

    Completed interative sorting and attempted stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,40 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
+        # (hint, can do in 3 loc)
+        for j in range(cur_index + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
         # TO-DO: swap
-
-
-
+        temp_value = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp_value
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+	swap = True
+	while swap == True:
+		swap = False
+		for i in range(0, len(arr) - 1):
+			if arr[i] > arr[i + 1]:
+				arr[i], arr[i + 1] = arr[i + 1], arr[i]
+				swap = True
+	
 
-    return arr
+	return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
-
+def count_sort(arr, maximum=-1):
+	count = []
+	sort = []
+	for i in range(0, len(arr) - 1):
+		count[arr[i]] = count[arr[1]] + 1
     return arr
